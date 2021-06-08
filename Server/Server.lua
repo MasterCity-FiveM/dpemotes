2fd097d62f987c68f0188b55535325847e59af82
masterking32 <lichwow_masterking@yahoo.com>
2021-06-08 12:19:14 +0430

    ...

--- Server/Server.lua
+++ Server/Server.lua
@@ -5,11 +5,13 @@
 
 RegisterServerEvent("ServerEmoteRequest")
 AddEventHandler("ServerEmoteRequest", function(target, emotename, etype)
+	ESX.RunCustomFunction("anti_ddos", source, 'ServerEmoteRequest', {target = target, emotename = emotename, etype = etype})
 	TriggerClientEvent("ClientEmoteRequestReceive", target, emotename, etype)
 end)
 
 RegisterServerEvent("ServerValidEmote") 
 AddEventHandler("ServerValidEmote", function(target, requestedemote, otheremote)
+	ESX.RunCustomFunction("anti_ddos", source, 'ServerValidEmote', {target = target, requestedemote = requestedemote, otheremote = otheremote})
 	TriggerClientEvent("SyncPlayEmote", source, otheremote, source)
 	TriggerClientEvent("SyncPlayEmoteSource", target, requestedemote)
 end)
