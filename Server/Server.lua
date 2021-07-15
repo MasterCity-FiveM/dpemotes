b2a1e3d050153635a2b6e9a169a1a00be24e639c
masterking32 <lichwow_masterking@yahoo.com>
2021-07-15 23:06:43 +0430

    emote

--- Server/Server.lua
+++ Server/Server.lua
@@ -1,3 +1,6 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 -----------------------------------------------------------------------------------------------------
 -- Shared Emotes Syncing  ---------------------------------------------------------------------------
@@ -5,13 +8,13 @@
 
 RegisterServerEvent("ServerEmoteRequest")
 AddEventHandler("ServerEmoteRequest", function(target, emotename, etype)
-	ESX.RunCustomFunction("anti_ddos", source, 'ServerEmoteRequest', {target = target, emotename = emotename, etype = etype})
+	ESX.RunCustomFunction("anti_ddos", source, 'ServerEmoteRequest', {})
 	TriggerClientEvent("ClientEmoteRequestReceive", target, emotename, etype)
 end)
 
 RegisterServerEvent("ServerValidEmote") 
 AddEventHandler("ServerValidEmote", function(target, requestedemote, otheremote)
-	ESX.RunCustomFunction("anti_ddos", source, 'ServerValidEmote', {target = target, requestedemote = requestedemote, otheremote = otheremote})
+	ESX.RunCustomFunction("anti_ddos", source, 'ServerValidEmote', {})
 	TriggerClientEvent("SyncPlayEmote", source, otheremote, source)
 	TriggerClientEvent("SyncPlayEmoteSource", target, requestedemote)
 end)
